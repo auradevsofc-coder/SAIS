4e96270ba339e4d65e49b35cd358456cefcba0d6
isacGhilardi <isacghilardi2@gmail.com>
2026-02-04 09:48:56 -0300

    up : Estruturas e ambiente atualizados

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'prettier'],
+  ignorePatterns: ['dist'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  rules: { 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }], 'prettier/prettier': 'error', 'react/react-in-jsx-scope': 'off' },
+};
---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+﻿import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-primary-600">
+        SAIS Frontend Configurado! 🚀
+      </h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Toaster position="top-right" />
+    </>
+  )
+}
+
+export default App
