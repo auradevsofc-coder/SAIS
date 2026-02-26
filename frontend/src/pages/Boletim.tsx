0264c4cc3aee9d9109923366368ee98aa8124d8b
Icaro Mairon <icaromaironreal@gmail.com>
2026-02-26 08:59:15 -0300

    fix: corrigindo index.css, tailwind funcionando

---
+++ frontend/src/pages/Boletim.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from '../components/NavBar';
+import '../App.css';
+import '../index.css';   
+
+export function Boletim() {
+    return(
+        <>
+        <NavBar />
+        </>
+    ) 
+}
